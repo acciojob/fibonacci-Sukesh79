4314195ba0206026a3bde8e6dedb9f2f45806a95
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-07-14 18:11:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function fibonacci(num) {
-	let num1 = 0;
-	let num2 = 1;
-	let sum;
-	let i = 0;
-	for (i = 0; i < num; i++) {
-		sum = num1 + num2;
-		num1 = num2;
-		num2 = sum;
-	}
-	return num2;
+  if (num === 1) return 0;
+  else if (num === 2) return 1;
+  num -= 2;
+  var num1 = 0;
+  var num2 = 1;
+  var sum;
+  var i = 0;
+  for (i = 0; i < num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
 }
-
-// console.log("Fibonacci(5): " + fibonacci(5));
-// console.log("Fibonacci(8): " + fibonacci(8));
 
 module.exports = fibonacci;
